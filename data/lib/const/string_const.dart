68531833ecfe517f7fc88bedceb72c2a48465b4e
NikkoES <nikkoeka04@gmail.com>
2022-09-06 15:03:41 +0700

    fix pusher

--- data/lib/const/string_const.dart
+++ data/lib/const/string_const.dart
@@ -1,7 +1,5 @@
 class StringConst {
   static const String sortOrderByKey = "&sortKey=updated_at&sortOrder=desc";
   static const String perPageCount = "20";
-  static const String beemsToken = "68e75384-d005-420b-bf5f-438b5320fd59";
-
-  // 8d24ad93-bc09-4539-93ad-34d6e21e5c32
+  static const String beemsToken = "4b69bb1b-d2b5-422a-9375-c261deffd766";
 }
